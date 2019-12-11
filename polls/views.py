101f6e867772c0929f4e85ff00f121df5354e0a4
Sisi <dqgsh1055@gmail.com>
2019-12-11 18:32:54 +0900

    3.2.9 여러 가지 뷰 추가하기

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from polls.models import Question
 
@@ -8,14 +8,10 @@ def index(request):
     latest_question_list = Question.objects.order_by(
         '-pub_date'
     )[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
-
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
