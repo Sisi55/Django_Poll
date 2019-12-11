9f9a11ca0d741d4f235e95c581d0918f7734f4ca
Sisi <dqgsh1055@gmail.com>
2019-12-11 19:55:05 +0900

    3.2.17-1  관리자 화면 목록 커스터마이징 관리자 화면 목록의 클래스 함수 출력 모양을 변경하기 위해 모델 코드를 수정합니다

--- polls/models.py
+++ polls/models.py
@@ -24,6 +24,14 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    was_published_recently.admin_order_field = 'pub_date' # 정렬 기준
+    # 원칙적으로 메서드에 의한 값은 정렬이 불가능함.
+    # 대신 다른 값을 기준으로 정렬할 수 있다
+    was_published_recently.boolean = True # 아이콘 모양
+    # 값이 불리언 값 형태인지 설정한다. True이면 값 대신 아이콘 출력된다
+    was_published_recently.short_description = 'Published recently?' # 인덱스 이름
+    # 항목의 헤더 이름을 설정한다
+
 
 class Choice(models.Model):
     question = models.ForeignKey(  # 포함되는 쪽에서 갖는다 ?
