3a3c5e1afac1a5d469769f855ce41d7374b29c3d
Sisi <dqgsh1055@gmail.com>
2019-12-11 19:44:45 +0900

    3.2.16-1  관리자 폼 커스터마이징 관리자 화면을 커스터마이징 하기 위해 admin.py 에서 ModelAdmin 을 상속받는 클래스를 만들고 register() 인자2에 전달합니다

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 
-from polls.models import Question
-# Register your models here.
-# 만든 모델을 관리하려면 등록해야 한다
-admin.site.register(Question)
+from polls.models import Question, Choice
+
+
+class ChoiceInline(admin.StackedInline):
+    """
+    답변 항목도 같이 등록하고 수정할 수 있게 추가한다.
+    Choice 모델을 위한 옵션 클래스를 만드는데
+    StackedInline 클래스를 상속받는다
+    만든 클래스를 QustionAdmin 클래스의 inlines 클래스 변수에 추가한다
+    """
+    model = Choice
+    extra = 3  # 화면에서 몇 개가 보이는지 ?
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    fieldsets 변수를 이용해 입력/수정 화면에서
+    각 항목들을 그룹화 하고, 그룹의 이름을 설정한다
+    (그룹이름, 그룹화항목들) ?
+    """
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
